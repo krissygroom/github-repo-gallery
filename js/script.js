0bf48aba43c5fdd5f1ac3679a70d56ab49e13751
krissy <krissydgroom@gmail.com>
2021-12-14 17:46:55 -0800

    Added functions to fetch and display repos list

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // SELECT DOM ELEMENTS
-
 // Section where user profile info will appear
 const overview = document.querySelector(".overview");
+
+// Unordered list to display repos list
+const repoList = document.querySelector(".repo-list");
 
 // Github username
 const username = "krissygroom"
@@ -12,7 +14,7 @@ async function getGithubInfo() {
     try {
         const response = await fetch(apiUrl);
         // convert response into json object
-        userInfo = await response.json();
+        const userInfo = await response.json();
         // console.log(userInfo);
         displayUserInfo(userInfo);
     } catch (error) {
@@ -45,4 +47,34 @@ function displayUserInfo(userInfo) {
 };
 
 
+// Async function to fetch list of repos
+async function getRepos() {
+    const apiUrl = `https://api.github.com/users/${username}/repos?sort=created&per_page=100`;
+    try {
+        const response = await fetch(apiUrl);
+        const allRepos = await response.json();
+        console.log(allRepos);
+        displayRepos(allRepos);
+    } catch (error) {
+        alert(error);
+        // Catch  error here if
+        // not able to fetch user data for any reason
+        console.log('Unable to access user info');
+    };
+};
+
+
+// Display info about each repo
+function displayRepos(repos) {
+    repos.forEach(repo => {
+        let li = document.createElement('li');
+        li.setAttribute("class", "repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`
+        repoList.append(li);
+    });
+};
+
+
 getGithubInfo();
+
+getRepos();
