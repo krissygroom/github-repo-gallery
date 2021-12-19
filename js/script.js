8405dd08f49c67cacb4b395c3f5e8ed906bce319
krissy <krissydgroom@gmail.com>
2021-12-19 13:18:21 -0800

    Added display repo functionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-// SELECT DOM ELEMENTS
+// *** GLOBAL VARIABLES ***
 // Section where user profile info will appear
 const overview = document.querySelector(".overview");
 
 // Unordered list to display repos list
 const repoList = document.querySelector(".repo-list");
 
-// Github username
-const username = "krissygroom"
+// Repos section
+const reposSection = document.querySelector(".repos");
 
+// Section where individual repo data will appear
+const repoData = document.querySelector(".repo-data");
+
+// Github username
+const username = "krissygroom";
+
+
+
+// *** FUNCTIONS ***
 // Async function to fetch github info
 async function getGithubInfo() {
     const apiUrl = `https://api.github.com/users/${username}`;
@@ -19,8 +28,7 @@ async function getGithubInfo() {
         displayUserInfo(userInfo);
     } catch (error) {
         alert(error);
-        // Catch  error here if
-        // not able to fetch user data for any reason
+        // Catch  error if unable to fetch user data for any reason
         console.log('Unable to access user info');
     };
 };
@@ -53,13 +61,10 @@ async function getRepos() {
     try {
         const response = await fetch(apiUrl);
         const allRepos = await response.json();
-        console.log(allRepos);
         displayRepos(allRepos);
     } catch (error) {
         alert(error);
-        // Catch  error here if
-        // not able to fetch user data for any reason
-        console.log('Unable to access user info');
+        console.log('Unable to get repos');
     };
 };
 
@@ -75,6 +80,54 @@ function displayRepos(repos) {
 };
 
 
+// Click event on h3 name of repo
+repoList.addEventListener('click', function(e) {
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText
+        getRepoInfo(repoName);
+    };
+});
+
+
+// Get specific repo info when name is clicked
+async function getRepoInfo(repoName) {
+    const apiUrl = `https://api.github.com/repos/${username}/${repoName}`;
+    let languages = []
+    try {
+        const response = await fetch(apiUrl);
+        const repoInfo = await response.json();
+        const fetchLanguages = await fetch(repoInfo.languages_url);
+        const languageData = await fetchLanguages.json();
+        for (let lang in languageData) {
+            languages.push(lang);
+        };
+        displayRepoInfo(repoInfo, languages);
+
+    } catch (error) {
+        alert(error);
+        console.log('Unable to get repo info');
+    };
+};
+
+
+// Display individual repo info when name is clicked
+function displayRepoInfo(repoInfo, languages) {
+    repoData.innerHTML = "";
+    let repoInfoDiv = document.createElement("div");
+    repoInfoDiv.innerHTML = `
+    <h3><strong>Name</strong>: ${repoInfo.name}</h3>
+        <p><strong>Description</strong>: ${repoInfo.description}</p>
+        <p><strong>Default Branch</strong>: ${repoInfo.default_branch}</p>
+        <p><strong>Languages</strong>: ${languages.join(", ")}</p>
+        <a class="visit" href="${repoInfo.svn_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+    `;
+    repoData.append(repoInfoDiv);
+    repoData.classList.remove("hide");
+    reposSection.classList.add("hide");
+};
+
+
+// *** FUNCTION CALLS ***
 getGithubInfo();
 
 getRepos();
